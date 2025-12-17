43d1e0d510000f31a074d75c8135cc35c4a7ef84
Andrei Mihai <user.andrei@yahoo.com>
2025-12-17 10:19:48 +0200

    Make the docker build run at each commit

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 
 class Settings:
     APP_NAME = "teacher_agent"
-    APP_VERSION = "1.7.0"
+    APP_VERSION = "1.8.0"
     USER_ID = "user"
     BASE_URL = os.getenv("BASE_URL", "http://localhost:8082")
     BASE_DIR = Path(__file__).parent
